5c8878162bbe84c74f07b306acf571fe2b235991
Marcosdaag <marcosoffs99@gmail.com>
2025-11-13 10:43:49 -0300

    tiempo de sesion

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ var authController = {
             const token = jwt.sign(
                 { id: user._id, username: user.username, role: user.role },
                 process.env.JWT_SECRET,
-                { expiresIn: '1h' }
+                { expiresIn: '4h' }
             );
 
             res.json({
